a7df5d135eb64b2d4bd72100c0bf0260b4f9e59a
joe werderber <greendog22@gmail.com>
2017-05-05 09:27:52 -0700

    added basic html, specs and readme.md

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+document.ready()
+// make sure number is positive
+
+
+// count to number inputted by user
+
+// replace number divislbe by 3 with ping
+
+// repalce numbers divisible by 5 with pong
+
+// replace numbers divivsible by 15 with pingpong
