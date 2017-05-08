1522392b1fd126a3a2985b863e47a8937da78010
joe werderber <greendog22@gmail.com>
2017-05-08 09:47:10 -0700

    it works

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,40 @@
-// make sure number is positive
-function positiveNumber (input) {
-  if (input > 0 ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-console.log("ddd")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// count to number inputted by user
-
-// replace number divislbe by 3 with ping
-
-// repalce numbers divisible by 5 with pong
-
-// replace numbers divivsible by 15 with pingpong
-
-
 $(document).ready(function() {
   // submit is method
   $("form#user-input").submit(function(event) {
-    event.preventDefault();
-    var input = parseInt($("input#number").val());
+      event.preventDefault();
+      var input = parseInt($("input#number").val());
+      var resultsDiv = $('#results');
+      resultsDiv.empty();
+      //do something with positive
+      if(input > 0){
+          for (var i=1;i<=input;i++)
+          {
+              if((i%3)===0)
+              {
+                  //console.log("ping");
+                  resultsDiv.append('<span>ping</span><br>');
+              }
+              else if((i%5)===0)
+              {
+                  //console.log("ping");
+                  resultsDiv.append('<span>pong</span><br>');
+              }
+              else if((i%15)===0)
+              {
+                  //console.log("pingpong");
+                  resultsDiv.append('<span>pingpong</span><br>');
+              }
+              else
+              {
+                  //console.log(i);
+                  resultsDiv.append('<span>' + i + '</span><br>');
+              }
+          }
+      }
+      //do something with negative values
+      else {
+          resultsDiv.append('<span>Please Enter a positive number.</span>');
 
-
-    var output = positiveNumber(input);
-
-    $(results).append(positiveNumber);
-    console.log("ddd")
-
+      }
   });
 });
